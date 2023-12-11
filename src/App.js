29a8100a0c5951db000ae9cdf0a47b26934890e0
Luis Manyari <lmanyari@hotmail.com>
2023-12-10 20:41:25 -0500

    :P~ First Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
+import React from "react";
 import ReactPlayer from "react-player";
 
-function App() {
+/* function App() {
   const vidUrl = "https://www.facebook.com/facebook/videos/10153231379946729/";
   return (
     <div className="App">
@@ -15,4 +16,42 @@ function App() {
   );
 }
 
+export default App; */
+
+
+function App() { 
+ 
+  const bird1 = new Audio( 
+    "https://upload.wikimedia.org/wikipedia/commons/9/9b/Hydroprogne_caspia_-_Caspian_Tern_XC432679.mp3" 
+  ); 
+ 
+  const bird2 = new Audio( 
+    "https://upload.wikimedia.org/wikipedia/commons/b/b5/Hydroprogne_caspia_-_Caspian_Tern_XC432881.mp3" 
+  ); 
+ 
+  function toggle1() { 
+    if (bird1.paused) { 
+      bird1.play(); 
+    } else { 
+      bird1.pause(); 
+    } 
+  }; 
+ 
+  function toggle2() { 
+    if (bird2.paused) { 
+      bird2.play(); 
+    } else { 
+      bird2.pause(); 
+    } 
+  }; 
+ 
+  return ( 
+    <div> 
+      <button onClick={toggle1}>Caspian Tern 1</button> 
+      <button onClick={toggle2}>Caspian Tern 2</button> 
+    </div> 
+  ); 
+} 
+ 
 export default App;
+
