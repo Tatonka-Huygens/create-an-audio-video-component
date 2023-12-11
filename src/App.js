9249d9e4d7bd6cadd825375a5a499f914adb027a
Luis Manyari <lmanyari@hotmail.com>
2023-12-10 19:46:24 -0500

    :P~ First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import ReactPlayer from "react-player";
 
 function App() {
+  const vidUrl = "https://www.facebook.com/facebook/videos/10153231379946729/";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>React Player example</h1>
+        <ReactPlayer
+          url={vidUrl}
+          playing={false}
+          volume={0.5}
+        />
     </div>
   );
 }
